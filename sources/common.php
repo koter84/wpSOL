6dad53e5740f9069cbbd7a426a5cdafada9d0bc8
Dennis Koot <koter84@gmail.com>
2017-03-20 09:00:47 +0100

    scrutinizer fixes

--- sources/common.php
+++ sources/common.php
@@ -14,7 +14,7 @@
 // Login Formulier
 function wpsol_wp_login_form_middle() // login_form_middle filter
 {
-	return wpsol_wp_login_form(array('echo'=>false,'sidebar'=>true));
+	return wpsol_wp_login_form(array('echo'=>false, 'sidebar'=>true));
 }
 function wpsol_wp_login_form($args = array()) // login_form action
 {
@@ -22,17 +22,17 @@ function wpsol_wp_login_form($args = array()) // login_form action
 		'echo' => true,
 		'sidebar' => false,
 	);
-	$args = wp_parse_args( $args, $defaults );
+	$args = wp_parse_args($args, $defaults);
 
 	$echo = "";
 
-	if( !$args['sidebar'] )
+	if(!$args['sidebar'])
 		$echo .= '<hr id="openid_split" style="clear: both; margin-bottom: 1.0em; border: 0; border-top: 1px solid #999; height: 1px;" />';
 
 	$echo .= '
 	<style>
 	#openid_enabled_link, .openid_link, #openid_identifier, #commentform #openid_identifier {
-		background-image: url(\'' . plugins_url( 'scnllogo.png', __FILE__ ) .  '\');
+		background-image: url(\''.plugins_url('scnllogo.png', __FILE__).'\');
 		background-position: 3px 50%;
 		background-repeat: no-repeat;
 		padding-left: 21px !important;
@@ -43,7 +43,7 @@ function wpsol_wp_login_form($args = array()) // login_form action
 		<input type="text" name="openid_identifier" id="openid_identifier" class="input openid_identifier" value="" size="20" /></label>
 	</p>';
 
-	if( $args['echo'] )
+	if($args['echo'])
 		echo $echo;
 	else
 		return $echo;
@@ -54,9 +54,9 @@ function wpsol_sidebar_login()
 {
 	$result = "";
 
-	if( is_user_logged_in() )
+	if(is_user_logged_in())
 	{ // ingelogd
-		if( get_option('wpsol_widget_links_show') )
+		if(get_option('wpsol_widget_links_show'))
 		{
 			$result = "<h1 class=\"widget-title\">".__('Members-Area', 'wpsol')."</h1>";
 
@@ -91,7 +91,7 @@ function wpsol_sidebar_login()
 function wpsol_sidebar_login_control()
 {
 	//the form is submitted, save into database
-	if (isset($_POST['submitted']))
+	if(isset($_POST['submitted']))
 	{
 		update_option('wpsol_widget_links_show', $_POST['widget_links_show']);
 	}
@@ -111,31 +111,31 @@ function wpsol_authenticate_username_password()
 	# get domain-name from wordpress-settings
 	$openid = new LightOpenID(get_site_url());
 
-	if( array_key_exists('openid_identifier', $_POST) && $_POST['openid_identifier'] )
+	if(array_key_exists('openid_identifier', $_POST) && $_POST['openid_identifier'])
 	{ // Attempt to authenticate user
 		try
 		{
-			if( !$openid->mode )
+			if(!$openid->mode)
 			{
-				if( isset( $_POST['openid_identifier'] ) )
+				if(isset( $_POST['openid_identifier']))
 				{
-					$openid->identity = 'https://login.scouting.nl/user/' . $_POST['openid_identifier'];
+					$openid->identity = 'https://login.scouting.nl/user/'.$_POST['openid_identifier'];
 					# The following two lines request email, full name, and a nickname
 					# from the provider. Remove them if you don't need that data.
 					$openid->required = array('contact/email','namePerson', 'namePerson/friendly');
-					$openid->optional = array('birthDate','person/gender','contact/postalCode/home','contact/country/home','pref/language','pref/timezone');
-					header('Location: ' . $openid->authUrl());
+					$openid->optional = array('birthDate', 'person/gender', 'contact/postalCode/home', 'contact/country/home', 'pref/language', 'pref/timezone');
+					header('Location: '.$openid->authUrl());
 				}
 			}
 		}
-		catch( ErrorException $e )
+		catch(ErrorException $e)
 		{
-			return new WP_Error( 'exception_error', "<strong>ERROR</strong>: " . $e->getMessage() );
+			return new WP_Error('exception_error', "<strong>ERROR</strong>: ".$e->getMessage());
 		}
 	}
-	if( $openid->mode )
+	if($openid->mode)
 	{
-		if( $openid->validate() )
+		if($openid->validate())
 		{
 			$new_user = false;
 
@@ -143,17 +143,17 @@ function wpsol_authenticate_username_password()
 			$username = get_option('wpsol_username_prefix').$gegevens['namePerson/friendly'];
 			$email = $gegevens['contact/email'];
 
-			$user_id = username_exists( $username );
-			$email_id = email_exists( $email );
+			$user_id = username_exists($username);
+			$email_id = email_exists($email);
 
-			if( !$user_id && !$email_id )
+			if(!$user_id && !$email_id)
 			{ // geen user_id, geen email_id, create new user.
-				if( get_option('wpsol_autocreate') )
+				if(get_option('wpsol_autocreate'))
 				{
-					$random_password = wp_generate_password( 18, false );
-					$user_id = wp_create_user( $username, $random_password, $email );
+					$random_password = wp_generate_password(18, false);
+					$user_id = wp_create_user($username, $random_password, $email);
 					$new_user = true;
-					$user = get_user_by( 'email', $email );
+					$user = get_user_by('email', $email);
 				}
 				else
 				{
@@ -162,22 +162,22 @@ function wpsol_authenticate_username_password()
 					return false;
 				}
 			}
-			elseif( !$user_id )
+			elseif(!$user_id)
 			{ // geen user_id, wel email_id, login
 				// gebruiker bestaat maar met een andere username dan bij SOL, bijvoorbeeld de site beheerder o.i.d.
-				$user = get_user_by( 'id', $email_id );
+				$user = get_user_by('id', $email_id);
 			}
-			elseif( !$email_id )
+			elseif(!$email_id)
 			{ // geen email_id, wel user_id, login
-				$user = get_user_by( 'id', $user_id );
+				$user = get_user_by('id', $user_id);
 				// update email voor de user, aangezien die blijkbaar veranderd is
-				wp_update_user( array('ID' => $user_id, 'user_email' => $email) );
+				wp_update_user(array('ID' => $user_id, 'user_email' => $email));
 			}
-			elseif( $user_id == $email_id )
+			elseif($user_id == $email_id)
 			{ // login.
-				$user = get_user_by( 'id', $user_id );
+				$user = get_user_by('id', $user_id);
 			}
-			elseif( $user_id != $email_id )
+			elseif($user_id != $email_id)
 			{ // user_id en email_id komen niet overeen
 				global $error;
 				$error = sprintf(__('wp-user-id based on username (%s) does not match wp-user-id based on email (%s)', 'wpsol'), $username, $email).'<br/><a href="https://wordpress.org/plugins/wpsol/installation/">'.__('wpSOL Setup Instructions', 'wpsol').'</a>';
@@ -190,9 +190,9 @@ function wpsol_authenticate_username_password()
 				return false;
 			}
 
-			if( $new_user || get_option('wpsol_force_display_name') )
+			if($new_user || get_option('wpsol_force_display_name'))
 			{
-				switch( get_option('wpsol_display_name') )
+				switch(get_option('wpsol_display_name'))
 				{
 					case 'firstname':
 						$display_name = substr($gegevens['namePerson'], 0, strpos($gegevens['namePerson'], " ") );
@@ -209,18 +209,18 @@ function wpsol_authenticate_username_password()
 						break;
 				}
 
-				update_user_meta( $user->ID, 'nickname', $display_name );
-				wp_update_user( array( 'ID' => $user->ID, 'display_name' => $display_name ) );
+				update_user_meta($user->ID, 'nickname', $display_name);
+				wp_update_user(array('ID' => $user->ID, 'display_name' => $display_name));
 			}
 
-			if( $new_user || get_option('wpsol_force_first_last_name') )
+			if($new_user || get_option('wpsol_force_first_last_name'))
 			{
-				update_user_meta( $user->ID, 'first_name', substr($gegevens['namePerson'], 0, strpos($gegevens['namePerson'], " ") ) );
-				update_user_meta( $user->ID, 'last_name', substr($gegevens['namePerson'], strpos($gegevens['namePerson'], " ")+1 ) );
+				update_user_meta($user->ID, 'first_name', substr($gegevens['namePerson'], 0, strpos($gegevens['namePerson'], " ")));
+				update_user_meta($user->ID, 'last_name', substr($gegevens['namePerson'], strpos($gegevens['namePerson'], " ")+1 ));
 			}
 
 			// add login filter to redirect
-			add_filter( 'login_redirect', 'wpsol_login_redirect' );
+			add_filter('login_redirect', 'wpsol_login_redirect');
 
 			return $user;
 		}
@@ -255,11 +255,11 @@ function wpsol_install()
 // Redirect after login
 function wpsol_login_redirect()
 {
-	if( get_option('wpsol_login_redirect') == "frontpage" )
+	if(get_option('wpsol_login_redirect') == "frontpage")
 	{
 		return "/";
 	}
-	elseif( get_option('wpsol_login_redirect') == "dashboard" )
+	elseif(get_option('wpsol_login_redirect') == "dashboard")
 	{
 		return "/wp-admin/";
 	}
@@ -268,15 +268,16 @@ function wpsol_login_redirect()
 // Redirect after logout
 function wpsol_logout_redirect()
 {
-	if( get_option('wpsol_logout_redirect') == "frontpage" )
+	if(get_option('wpsol_logout_redirect') == "frontpage")
 	{
-		wp_redirect( home_url() );
+		wp_redirect(home_url());
 		exit;
 	}
 }
 
 // Geef extra links op de plugin-overzichtspagina
-function wpsol_plugin_action_links( $links ) {
+function wpsol_plugin_action_links($links)
+{
 	array_unshift($links, '<a href="'.esc_url(get_admin_url(null, 'options-general.php?page=wpsol_settings')).'">'.__('Settings', 'wpsol').'</a>');
 	return $links;
 }
@@ -284,9 +285,9 @@ function wpsol_plugin_action_links( $links ) {
 // Admin Settings Pagina
 function wpsol_admin_options()
 {
-	if ( !current_user_can( 'manage_options' ) )
+	if(!current_user_can('manage_options'))
 	{
-		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+		wp_die(__('You do not have sufficient permissions to access this page.'));
 	}
 
 	// variables for the field and option names
@@ -339,26 +340,26 @@ function wpsol_admin_options()
 
 	// See if the user has posted us some information
 	$hidden_field_name = 'wpsol_hidden';
-	if( isset($_POST[ $hidden_field_name ]) && $_POST[ $hidden_field_name ] == 'Y' )
+	if(isset($_POST[ $hidden_field_name ]) && $_POST[ $hidden_field_name ] == 'Y')
 	{
 		foreach($options as $key => $opt)
 		{
 			// Save the posted value in the database
 			if(isset($_POST[$key]))
 			{
-				update_option( $key, $_POST[$key] );
+				update_option($key, $_POST[$key]);
 			}
 			elseif($opt['type'] == "checkbox")
 			{
-				update_option( $key, 0 );
+				update_option($key, 0);
 			}
 		}
 		// Put an settings updated message on the screen
 		echo "<div class=\"updated\"><p><strong>".__('Settings Saved', 'wpsol')."</strong></p></div>";
-    }
+	}
 
-    // Now display the settings editing screen
-    ?>
+	// Now display the settings editing screen
+	?>
 <div class="wrap">
 	<div id="icon-options-general" class="icon32"><br></div>
 	<h2><?php _e('wpSOL [ScoutsOnLine] Settings', 'wpsol'); ?></h2>
